a1e8b5d0b3ba4c89d6ccd50f07c48f578593ebae
Michael Seger <michael@macbookpro>
2020-09-21 15:43:25 +0200

    updated filter

--- run/filter-tcp-logs.sh
+++ run/filter-tcp-logs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 dir=.
 
 if [[ "$#" == 1 && -d "$1" ]] ; then
@@ -9,18 +11,19 @@ elif [ "$#" -ge 1 ]; then
   exit 1
 fi
 
-date_pat="^....-..-..T..:..:.........Z: up:"
+date_pat="2[0-9]{3}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{6}Z: [ud][pown]"
 end_pat="# END --------------------------"
-grep -a "$date_pat" "$dir"/*tcp*log | \
+grep -E -a "$date_pat" "$dir"/*tcp*log | \
   sed -e "s/: .*//;s/log:/log\t/" | sort -u|\
 while read -r line ; do
   file=$(echo "$line"|cut -f1)
   start_pat=$(echo "$line"|cut -f2)
   type=$(echo "$file"|sed -e "s#.*/##;s/.tcp.log$//;s/[0-9]*-//")
-  grep -A150 -a "^$start_pat" "$file" | \
+  grep -A350 -a "^$start_pat" "$file" | \
     sed -n "/^$start_pat/,/$end_pat/p;/$end_pat/q" | \
     grep -i -v -E -e "^(X-|Cache|Pragma|Expire|Connection:|\
       |Keep-Alive|Upgrade|DNT|Accept|Referer|Transfer|Sec-|[[:space:]]*$|Content-Length: 0|Vary|Last-Modified|0)" | \
-    sed -e "s/$start_pat/\n$type\n$start_pat\n/;s/TCP Forwarding //;/^: up:$/d;/ stopped.$/d;s/.*: down:/---->/" |\
-    grep -E -v "(^: up:$|$date_pat)"
+    sed -e "s/$start_pat/\n$type\n$start_pat /;s/TCP Forwarding //;s/ : up:/ ---->/;/ stopped.$/d;s/ : down:/ <----/"
+    #grep -E -v "(^: up:$|$date_pat)"
+#  break
 done
